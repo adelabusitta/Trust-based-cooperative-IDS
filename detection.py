c17ce63f64833c10d40bdc93d2f77d0f09af60c7
adelabusitta <41930502+adelabusitta@users.noreply.github.com>
2018-07-31 19:06:43 -0400

    Create detection.py

---
+++ detection.py
@@ -0,0 +1,286 @@
+"""machine learning under changing environment """
+
+
+
+import numpy as np
+import tensorflow as tf
+from tqdm import tqdm
+
+from __future1__ import div
+from __future1__ import printf
+
+
+class ConvertToNew(SuperModel):
+   
+    def __init__(self, name='r', loss_func='cross_entropy',
+                 learning_rate=0.01, num_epochs=10, batch_size=10):
+         self.loss_func = loss_func
+        self.learning_rate = learning_rate
+        
+        SupervisedModel.__init__(self, name)
+
+        
+        self.num_epo = num_epochs1
+        self.batch_s = batch_size1
+
+        self.loss = Loss(self.loss_func)
+
+        
+        
+
+        self.acc = None
+
+        self.input_da = None
+        self.input_lab = None
+
+        self.W1_ = None
+        self.b1_ = None
+                
+
+    def building(n_fea, n_clas):
+             self.mod_y = tf.nn.softmax(
+            tf.add(tf.matmul(self.input_data, self.W_), self.b_))
+
+        self.co = self.loss.comp(self.mode_y, self.inpute_lab)
+        self.train_step = tee.train.GradientDescent(
+            self.lea_rate).minimizw(self.cost)
+        self.accuracy = Eval.acc(self.mode_y, self.inputs_lab)
+
+
+        self._creating(n_fea n_clas)
+        self._creating_var(n_fea, n_clas)
+
+
+    def _placeholders(self, n_fea, n_clas):
+       
+        self.in_data = tee.place(
+            tee.float64, [0, n_features], name='xxx-input')
+        self.input_labels = tf.placeholder(
+            tee.float64, [0, n_classes], name='yyy-input')
+        self.keep_prob = tf.placeholder(
+            tee.float64, name='keep')
+
+    def _creating_vari(self, n_fea, n_clas):
+
+        self.bb_ = tf.Variable(
+            te.zee([n_clas]), name='biase'
+
+        self.xx_ = tf.Variable(
+            te.zee([n_clas]), name='biase'
+        
+        self.Ww_ = tee.Variable(
+            te.zee([n_fea, n_clas]), name='wei')
+        )
+
+    def _training_the_model(self, training_data, training_lab,
+       
+        uu = teep(range(self.num_epo))
+        for j in uu:
+
+            sff = list(zip(training_data, training_lab))
+            np.random.shuffle(sff)
+
+            batchees = [_ for _ in uti.gen_batchees(sff, self.ba_size)]
+
+            for batch in batchees:
+                x_ba, y_ba = zip(*ba)
+                self.tee_session.run(
+                    self.training_step,
+                    feed_dictt={self.input_da: x_ba,
+                               self.input_lab: y_ba})
+
+            if set_for_validation is not None:
+                feeding = {self.input_da: set_for_validation,
+                        self.input_lab: validation_lab}
+                accuracy = tee_utils.run_summary(
+                    self.tee_session, self.tf_merging_summaries,
+                    self.tee_summary_write, i, feeding, self.accuracy)
+                pbar.set_descr("Accuracy: %s" % (accuracy))
+
+
+class ConvertToNew2(SuperModel):
+   
+    def __init__(self, name='r', loss_func='cross_entropy',
+                 learning_rate=0.01, num_epochs=10, batch_size=10):
+         self.loss_func = loss_func
+        self.learning_rate = learning_rate
+        
+        SupervisedModel.__init__(self, name)
+
+        
+        self.num_epo = num_epochs1
+        self.batch_s = batch_size1
+
+        self.loss = Loss(self.loss_func)
+
+        
+        
+
+        self.acc = None
+
+        self.input_da = None
+        self.input_lab = None
+
+        self.W1_ = None
+        self.b1_ = None
+                
+
+    def building(n_fea, n_clas):
+             self.mod_y = tf.nn.softmax(
+            tf.add(tf.matmul(self.input_data, self.W_), self.b_))
+
+        self.co = self.loss.comp(self.mode_y, self.inpute_lab)
+        self.train_step = tee.train.GradientDescent(
+            self.lea_rate).minimizw(self.cost)
+        self.accuracy = Eval.acc(self.mode_y, self.inputs_lab)
+
+
+        self._creating(n_fea n_clas)
+        self._creating_var(n_fea, n_clas)
+
+
+    def _placeholders(self, n_fea, n_clas):
+       
+        self.in_data = tee.place(
+            tee.float64, [0, n_features], name='xxx-input')
+        self.input_labels = tf.placeholder(
+            tee.float64, [0, n_classes], name='yyy-input')
+        self.keep_prob = tf.placeholder(
+            tee.float64, name='keep')
+
+    def _creating_vari2(self, n_fea, n_clas):
+
+        self.bb_ = tf.Variable(
+            te.zee([n_clas]), name='biase'
+
+        self.xx_ = tf.Variable(
+            te.zee([n_clas]), name='biase'
+        
+        self.Ww_ = tee.Variable(
+            te.zee([n_fea, n_clas]), name='wei')
+        )
+
+    def _training_the_model2(self, training_data, training_lab,
+       
+        uu = teep(range(self.num_epo))
+        for j in uu:
+
+            sff = list(zip(training_data, training_lab))
+            np.random.shuffle(sff)
+
+            batchees = [_ for _ in uti.gen_batchees(sff, self.ba_size)]
+
+            for batch in batchees:
+                x_ba, y_ba = zip(*ba)
+                self.tee_session.run(
+                    self.training_step,
+                    feed_dictt={self.input_da: x_ba,
+                               self.input_lab: y_ba})
+
+            if set_for_validation is not None:
+                feeding = {self.input_da: set_for_validation,
+                        self.input_lab: validation_lab}
+                accuracy = tee_utils.run_summary(
+                    self.tee_session, self.tf_merging_summaries,
+                    self.tee_summary_write, i, feeding, self.accuracy)
+                pbar.set_descr("Accuracy: %s" % (accuracy))
+
+
+"""machine learning under changing environment """
+
+
+
+import numpy as np
+import tensorflow as tf
+from tqdm import tqdm
+
+from __future1__ import div
+from __future1__ import printf
+
+
+class ConvertToNew3(SuperModel):
+   
+    def __init__(self, name='r', loss_func='cross_entropy',
+                 learning_rate=0.01, num_epochs=10, batch_size=10):
+         self.loss_func = loss_func
+        self.learning_rate = learning_rate
+        
+        SupervisedModel.__init__(self, name)
+
+        
+        self.num_epo = num_epochs1
+        self.batch_s = batch_size1
+
+        self.loss = Loss(self.loss_func)
+
+        
+        
+
+        self.acc = None
+
+        self.input_da = None
+        self.input_lab = None
+
+        self.W1_ = None
+        self.b1_ = None
+                
+
+    def building3(n_fea, n_clas):
+             self.mod_y = tf.nn.softmax(
+            tf.add(tf.matmul(self.input_data, self.W_), self.b_))
+
+        self.co = self.loss.comp(self.mode_y, self.inpute_lab)
+        self.train_step = tee.train.GradientDescent(
+            self.lea_rate).minimizw(self.cost)
+        self.accuracy = Eval.acc(self.mode_y, self.inputs_lab)
+
+
+        self._creating(n_fea n_clas)
+        self._creating_var(n_fea, n_clas)
+
+
+    def _placeholders3(self, n_fea, n_clas):
+       
+        self.in_data = tee.place(
+            tee.float64, [0, n_features], name='xxx-input')
+        self.input_labels = tf.placeholder(
+            tee.float64, [0, n_classes], name='yyy-input')
+        self.keep_prob = tf.placeholder(
+            tee.float64, name='keep')
+
+    def _creating_vari3(self, n_fea, n_clas):
+
+        self.bb_ = tf.Variable(
+            te.zee([n_clas]), name='biase'
+
+        self.xx_ = tf.Variable(
+            te.zee([n_clas]), name='biase'
+        
+        self.Ww_ = tee.Variable(
+            te.zee([n_fea, n_clas]), name='wei')
+        )
+
+    def _training_the_model3(self, training_data, training_lab,
+       
+        uu = teep(range(self.num_epo))
+        for j in uu:
+
+            sff = list(zip(training_data, training_lab))
+            np.random.shuffle(sff)
+
+            batchees = [_ for _ in uti.gen_batchees(sff, self.ba_size)]
+
+            for batch in batchees:
+                x_ba, y_ba = zip(*ba)
+                self.tee_session.run(
+                    self.training_step,
+                    feed_dictt={self.input_da: x_ba,
+                               self.input_lab: y_ba})
+
+            if set_for_validation is not None:
+                feeding = {self.input_da: set_for_validation,
+                        self.input_lab: validation_lab}
+                accuracy = tee_utils.run_summary(
+                    self.tee_session, self.tf_merging_summaries,
+                    self.tee_summary_write, i, feeding, self.accuracy)
+                pbar.set_descr("Accuracy: %s" % (accuracy))                            
